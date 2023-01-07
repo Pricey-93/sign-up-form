e982f62516c13994a3c6a9c3cc635c8e1d64dccd
dan <danieljamieprice@gmail.com>
2023-01-08 00:40:16 +0900

    add password validation

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
-function passwordCheck() {
-    /// 
+const passwordField = document.getElementById("password");
+const confirmPasswordField = document.getElementById("confirm-password");
+
+function comparePasswords() {
+    const password = document.getElementById("password").value.trim();
+    const confirmPassword = document.getElementById("confirm-password").value.trim();
+    if (password === confirmPassword) {
+        return true;
+    }
+    else {
+        return false;
+    }   
 }
+
+// if password strings match -> value is valid, else invalid.
+function validate() {
+    let confirmPasswordField = document.getElementById("confirm-password");
+    if (comparePasswords()) {
+        confirmPasswordField.setCustomValidity("");
+    } else {
+        confirmPasswordField.setCustomValidity("passwords must match");
+    }
+    confirmPasswordField.reportValidity();
+}
+
+// event listeners ---------------------------------------------------------
+confirmPasswordField.addEventListener("blur", (e) => {
+    validate();
+});
